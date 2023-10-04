c83565650b96ec10ced0a30610b9f70d42b472d5
Doang <doangcespedesloreto@gmail.com>
2023-10-04 10:15:29 -0400

    creamos el footer

--- src/views/MyWork/MyWork.jsx
+++ src/views/MyWork/MyWork.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Services } from '../../Core/infrastructure/components/sections/services/services'
 import { Slider } from '../../Core/infrastructure/components/carousels/slider/Slider'
+import { Footer } from '../../Core/infrastructure/components/footer/Footer'
 
 export const MyWork = () => {
   return (
     <div>
       <Slider/>
       <Services/>
+      <Footer/>
     </div>
   )
 }
