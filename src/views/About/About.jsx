f47b0f3bd076249647245c44a0ade4e919bb70dd
Doang <doangcespedesloreto@gmail.com>
2023-10-16 11:34:50 -0400

    trabajando el about

--- src/views/About/About.jsx
+++ src/views/About/About.jsx
@@ -1,19 +1,163 @@
+import CoustomSlider from '../../Core/infrastructure/components/carousels/coustomSlider/CoustomSlider'
 import './Style.css'
-import {GiAutoRepair} from "react-icons/gi";
 
 
 export const About = () => {
   return (
     <>
-
-        
-      <div  className="bg-dark">
-        <div className="container text-white text-center"> 
-          <h2>creating</h2>
-          <div className="fs-1 fw-bold "><GiAutoRepair/></div>
+       <div className="bg-dark text-center text-white">      
+        <div className="container">
+          <div>
+            <h2>Hail Repair: Precision in Every Dent</h2>
+            <p>At Lujancar Dent Shop, we're experts in the art of Hail Repair, a specialized technique used to restore vehicles that have been affected by hail damage. Hail Repair is a meticulous and precise process that demands a deep understanding of vehicle structures, paintwork, and specialized tools.</p>
+          </div>
           
+          
+            <div>
+              <h2>The Hail Repair Process</h2>
+              <p>Hail Repair, also known as Paintless Dent Repair (PDR), is a non-invasive method of repairing hail dents without the need for repainting. The process consists of several critical steps:</p>
+            </div>
+
+            
+
+          <div className="row">
+            <div className="col-md-4">
+              <div className="row">
+                <h4>Assessment:</h4>
+                <p>The first step is to assess the extent of the hail damage on the vehicle. Our skilled technicians carefully inspect each dent to determine the best approach for repair.</p>
+               </div>
+            </div>
+
+            <div className="col-md-4">
+              <div className="row">
+                <h4>Access:</h4>
+                <p>To access the dented area, the technician removes interior panels, weather stripping, or other components. This is necessary to reach the backside of the dent.</p>
+              </div>
+            </div>
+
+            <div className="col-md-4">
+              <div className="row">
+                <h4>Tools and Technique:</h4>
+                <p>Specialized tools, including metal rods and hand-operated hammers, are used to gently massage the dent from the inside, pushing it back into its original shape. The technician's expertise and precision are crucial at this stage to avoid damaging the paint.</p>            
+              </div>
+            </div>
+
+            <div className="col-md-4">
+              <div className="row">
+                <h4>Finishing Touch:</h4>
+                <p>Once the dent is restored, any imperfections or high spots are carefully smoothed out to ensure a seamless finish.</p>
+              </div>
+            </div>
+
+            <div className="col-md-4">
+              <div className="row">
+                <h4>Quality Inspection:</h4>
+                <p>After the repair is completed, a thorough quality inspection is performed to ensure that the dents are completely removed, leaving no visible signs of damage.</p>
+              </div>
+            </div>
+
+            <div className="col-md-4">
+              <div className="row">
+                <h4>Paint Protection:</h4>
+                <p>Because Paintless Dent Repair does not require repainting, the original paint finish is preserved, maintaining the vehicle's value and appearance.</p>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h2>Specialized Tools of the Trade</h2>
+            <p>Hail Repair professionals rely on a variety of specialized tools to perform their work with precision. Some of these tools include:</p>
+          </div>
+
+{/* ----------------------------------------------------------------------- */}
+
+          <div className="row">
+            <div className="col-md-4">
+              <div className="row">
+                <h4>Dent Removal Tools:</h4>
+                <p>These come in various shapes and sizes and are used to carefully push and massage teeth back into shape.</p>
+              </div>
+            </div>
+
+            <div className="col-md-4">
+              <div className="row">
+                <h4>Lighting Systems:</h4>
+                <p>Specialized lighting systems are used to highlight imperfections and ensure that the repair is flawless.</p>
+              </div>
+            </div>
+
+            <div className="col-md-4">
+              <div className="row">
+                <h4>Glue Pulling Tools:</h4>
+                <p>For dents that cannot be accessed from the backside, technicians use glue pulling tools to gently pull and reshape the dent.</p>
+              </div>
+            </div>
+
+            <div className="col-md-4">
+              <div className="row">
+                <h4>PDR Bridge Tools:</h4>
+                <p>These tools provide a stable base for applying pressure to dented areas and are essential for achieving smooth repairs.</p>
+              </div>
+            </div>
+
+            <div className="col-md-4">
+              <div className="row">
+                <br/>
+              </div>
+            </div>
+
+            <div className="col-md-4">
+              <div className="row">
+                <h4>Dent Removal Tabs:</h4>
+                <p>These adhesive tabs are used in conjunction with pulling tools to grip the dent and facilitate the repair process.</p>
+              </div>
+            </div>
+          </div>
+{/* ----------------------------------------------------------------------- */}
+
+<CoustomSlider/>
+
+          <div>
+            <h2>Why Choose Hail Repair?</h2>
+            <p>Hail Repair, or Paintless Dent Repair, is an environmentally friendly, cost-effective, and time-saving method for repairing hail damage. By choosing this technique, you can benefit from:</p>
+          </div>
+
+          <div className="row">
+            <div className="col-md-3">
+              <div className="row">
+                <h4>Preservation of Original Paint:</h4>
+                <p>Avoid repainting, which maintains the integrity and highlights value of your vehicle.</p>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="row">
+                <h4>Faster Repairs:</h4>
+                <p>Hail Repair typically takes less time than traditional bodywork, getting you back on the road sooner.</p>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="row">
+                <h4>Eco-Friendly:</h4>
+                <p>Hail Repair produces less waste, making it a sustainable choice.</p>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <div className="row">
+                <h4>Cost Savings:</h4>
+                <p>Lower repair costs compared to traditional methods.</p>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <h2>At Lujancar Dent Shop</h2>
+            <p>we take great pride in our expertise in Hail Repair. Our skilled technicians are dedicated to providing precise and high-quality repairs that restore your vehicle's beauty and value. Trust us to make hail damage a distant memory and experience the art of Hail Repair for yourself.</p>
+          </div>
+
         </div>
       </div>
+
+      <CoustomSlider/>
     </>
   )
 }
